e576da6ea9d9ca0232f0f4d598b7e9b40bc608ea
Brittany Ridley <britt.ridley@outlook.com>
2022-03-22 21:43:17 -0500

    master

--- CollectionsMasterConsoleUI/Program.cs
+++ CollectionsMasterConsoleUI/Program.cs
@@ -50,6 +50,7 @@ namespace CollectionsMasterConsoleUI
 
             Console.WriteLine("\n************End Arrays*************** \n");
             #endregion
+         
 
             #region Lists
             Console.WriteLine("************Start Lists**************");
@@ -91,10 +92,99 @@ namespace CollectionsMasterConsoleUI
             Console.WriteLine("------------------");
 
             //Convert the list to an array and store that into a variable
-            
+
 
             //Clear the list
-            
+
+
+            #endregion
+            #region Arrays
+            // Create an integer Array of size 50
+            var numbers = new int[50];
+
+            //Create a method to populate the number array with 50 random numbers that are between 0 and 50
+            Populater(numbers);
+
+            //Print the first number of the array
+
+            Console.WriteLine($"{numbers[0]}");
+
+            //Print the last number of the array            
+            Console.WriteLine($"{numbers[numbers.Length - 1]}");
+            Console.WriteLine("All Numbers Original");
+
+            //Use this method to print out your numbers from arrays or lists
+            NumberPrinter(numbers);
+            Console.WriteLine("-------------------");
+
+            //Reverse the contents of the array and then print the array out to the console.
+            //Try for 2 different ways
+            /*     Hint: Array._____(); Create a custom method     */
+
+            Console.WriteLine("All Numbers Reversed:");
+
+            Console.WriteLine("---------REVERSE CUSTOM------------");
+
+            Console.WriteLine("-------------------");
+
+            //Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
+            Console.WriteLine("Multiple of three = 0: ");
+
+
+            Console.WriteLine("-------------------");
+
+            //Sort the array in order now
+            /*      Hint: Array.____()      */
+            Console.WriteLine("Sorted numbers:");
+
+
+            Console.WriteLine("\n************End Arrays*************** \n");
+            #endregion
+
+            #region Lists
+            Console.WriteLine("************Start Lists**************");
+
+            /*   Set Up   */
+            //Create an integer List
+
+
+            //Print the capacity of the list to the console
+
+
+            //Populate the List with 50 random numbers between 0 and 50 you will need a method for this            
+
+
+            //Print the new capacity
+
+
+            Console.WriteLine("---------------------");
+
+            //Create a method that prints if a user number is present in the list
+            //Remember: What if the user types "abc" accident your app should handle that!
+            Console.WriteLine("What number will you search for in the number list?");
+
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("All Numbers:");
+            //Print all numbers in the list
+            //NumberPrinter();
+            Console.WriteLine("-------------------");
+
+            //Create a method that will remove all odd numbers from the list then print results
+            Console.WriteLine("Evens Only!!");
+
+            Console.WriteLine("------------------");
+
+            //Sort the list then print results
+            Console.WriteLine("Sorted Evens!!");
+
+            Console.WriteLine("------------------");
+
+            //Convert the list to an array and store that into a variable
+
+
+            //Clear the list
+
 
             #endregion
         }
@@ -122,7 +212,11 @@ namespace CollectionsMasterConsoleUI
 
         private static void Populater(int[] numbers)
         {
-            Random rng = new Random();
+           for(int i = 0; i < numbers.Length; i++)
+            {
+                Random rng = new Random();
+                numbers[i] = rng.Next(0, 50);
+            }
 
         }        
 
